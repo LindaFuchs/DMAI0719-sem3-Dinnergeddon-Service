d300c288e3c7f658e463f07e110134d64b42b9e6
Nikola Velichkov <nikola@velichkov-bg.com>
2018-11-08 09:49:44 +0100

    Added basic DB layer setup

---
+++ DBLayer/DBComponents.cs
@@ -0,0 +1,32 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.Common;
+
+namespace DBLayer
+{
+    public class DBComponents
+    {
+        private static DBComponents instance;
+        
+        private DBComponents()
+        {
+            string provider = ConfigurationManager.ConnectionStrings["auto"].ProviderName;
+            string connectionString = ConfigurationManager.ConnectionStrings["auto"].ConnectionString;
+
+            Factory = DbProviderFactories.GetFactory(provider);
+            Connection = Factory.CreateConnection();
+            Connection.ConnectionString = connectionString;
+        }
+
+        public static DBComponents GetInstance()
+        {
+            if (instance == null)
+                instance = new DBComponents();
+
+            return instance;
+        }
+        
+        public IDbConnection Connection { get; private set; }
+        public DbProviderFactory Factory { get; private set; }
+    }
+}

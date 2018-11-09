db18badeda114578fbd0fa2ea05f7b12db002f79
Nikola Velichkov <nikola@velichkov-bg.com>
2018-11-09 09:48:52 +0100

    Added WCF service

---
+++ DinnergeddonService/AccountService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DinnergeddonService
+{
+    public class AccountService : IAccountService
+    {
+        public bool Registration(string name, string email, string password, int id)
+        {
+            Console.WriteLine("Registering account with name {0}", name);
+            return true;
+        }
+    }
+}
---
+++ DinnergeddonService/IAccountService.cs
@@ -0,0 +1,11 @@
+﻿using System.ServiceModel;
+
+namespace DinnergeddonService
+{
+    [ServiceContract]
+    public interface IAccountService
+    {
+        [OperationContract]
+        bool Registration(string name, string email, string password, int id);
+    }
+}

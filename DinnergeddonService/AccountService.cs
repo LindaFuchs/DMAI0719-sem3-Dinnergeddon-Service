dc17e7340a5c52044fdd7f1f1b1370b67dbbe191
Nikola Velichkov <nikola@velichkov-bg.com>
2018-11-09 12:27:31 +0100

    Making AccountService implement IAccountService

--- DinnergeddonService/AccountService.cs
+++ DinnergeddonService/AccountService.cs
@@ -1,13 +1,43 @@
 ﻿using System;
+using Model;
 
 namespace DinnergeddonService
 {
     public class AccountService : IAccountService
     {
+        public bool CheckEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditAccount(string username, string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Account GetInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Register(string username, string email, string password, int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public bool Registration(string name, string email, string password, int id)
         {
-            Console.WriteLine("Registering account with name {0}", name);
-            return true;
+            throw new NotImplementedException();
         }
     }
 }

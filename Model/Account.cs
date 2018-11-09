18343ef26b88cf36808c71e44e97e46874a87e84
Nikola Velichkov <nikola@velichkov-bg.com>
2018-11-09 12:22:22 +0100

    Created model layer

---
+++ Model/Account.cs
@@ -0,0 +1,18 @@
+﻿namespace Model
+{
+    public class Account
+    {
+        private string username;
+        private string email;
+        private string password;
+        private int ID;
+
+        public Account(string username, string email, string password, int ID)
+        {
+            this.username = username;
+            this.email = email;
+            this.password = password;
+            this.ID = ID;
+        }
+    }
+}

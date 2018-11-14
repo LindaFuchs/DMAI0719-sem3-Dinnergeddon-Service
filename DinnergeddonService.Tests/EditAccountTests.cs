50e3570b0b605fa1a87f181cc07919781509a1bf
TheRealAnoobis <an00bis@abv.bg>
2018-11-14 13:30:48 +0100

    Revert "Completed Register Account tests"
    This reverts commit e7df151ce4e3f5679031108332d100d9f7a6144e.

--- DinnergeddonService.Tests/EditAccountTests.cs
+++ DinnergeddonService.Tests/EditAccountTests.cs
@@ -1,131 +1,50 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using DinnergeddonService;
 
 namespace DinnergeddonService.Tests
 {
     [TestClass]
     public class EditAccountTests
     {
-        String username = "us-er.name_1234";
+        String username = "testname";
+        String password = "testpass";
         String email = "testname@email.com";
-        String password = "testPass1@#$%";
 
 
         [TestMethod]
-        public void Test_EditAccount_All_Correct_Info_Succeeds()
+        public void CheckInfo_All_Correct_Info_Succeeds()
         {
             var infoChecker = new AccountService();
-            Assert.IsTrue(infoChecker.EditAccount(username, email, password));
+            Assert.IsTrue(infoChecker.EditAccount(username, password, email));
+        }
+
+        [TestMethod]
+        public void CheckInfo_Blank_Username_Fails()
+        {
+            var infoChecker = new AccountService();
+            Assert.IsFalse(infoChecker.EditAccount("", password, email));
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_EditAccount_Blank_Username_Throws_Exception()
+        public void CheckInfo_Blank_Password_Fails()
         {
             var infoChecker = new AccountService();
-            infoChecker.EditAccount("", email, password);
+            Assert.IsFalse(infoChecker.EditAccount(username, "", email));
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_EditAccount_Blank_Email_Throws_Exception()
+        public void CheckInfo_Blank_Email_Fails()
         {
             var infoChecker = new AccountService();
-            infoChecker.EditAccount(username, "", password);
+            Assert.IsFalse(infoChecker.EditAccount(username, password, ""));
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void Test_EditAccount_Blank_Password_Throws_Exception()
+        public void Test_Something()
         {
-            var infoChecker = new AccountService();
-            infoChecker.EditAccount(username, email, "");
-        }
 
-        [TestMethod]
-        public void Test_CheckEmail_Correct_Info_Passes()
-        {
-            var infoChecker = new AccountService();
-            Assert.IsTrue(infoChecker.CheckEmail(email));
-        }
-
-        [TestMethod]
-        public void Test_CheckEmail_Invalid_Format_Fails()
-        {
-            var infoChecker = new AccountService();
-            Assert.IsTrue(infoChecker.CheckEmail("john@mail"));
-        }
-
-        [TestMethod]
-        public void Test_CheckEmail_Blank_Fails()
-        {
-            var infoChecker = new AccountService();
-            Assert.IsFalse(infoChecker.CheckEmail("  "));
-        }
-
-        [TestMethod]
-        public void Test_CheckUsername_Correct_Info_Passes()
-        {
-            var infoChecker = new AccountService();
-            Assert.IsTrue(infoChecker.CheckUsername(username));
-        }
-
-        [TestMethod]
-        public void Test_CheckUsername_Characters_Over_Limit_Fails()
-        {
-            //32 char limit
-            var infoChecker = new AccountService();
-            Assert.IsFalse(infoChecker.CheckUsername("123456789012345678901234567890123"));
-        }
-
-        [TestMethod]
-        public void Test_CheckUsername_Characters_Under_Limit_Fails()
-        {
-            var infoChecker = new AccountService();
-            Assert.IsFalse(infoChecker.CheckUsername("us"));
-        }
-
-        [TestMethod]
-        public void Test_CheckUsername_Characters_Not_Permitted_Fails()
-        {
-            var infoChecker = new AccountService();
-            Assert.IsFalse(infoChecker.CheckUsername("ÆÆ"));
-        }
-
-        [TestMethod]
-        public void Test_CheckUsername_Blank_Fails()
-        {
-            var infoChecker = new AccountService();
-            Assert.IsFalse(infoChecker.CheckUsername("  "));
-        }
-
-        [TestMethod]
-        public void Test_CheckPassword_CorrectInfo_Passes()
-        {
-            var infoChecker = new AccountService();
-            Assert.IsTrue(infoChecker.CheckPassword(password));
-        }
-
-        [TestMethod]
-        public void Test_CheckPassword_Character_Over_Limit_Fails()
-        {
-            var infoChecker = new AccountService();
-            Assert.IsFalse(infoChecker.CheckPassword("1234567890123456789012345678901"));
-        }
-
-        [TestMethod]
-        public void Test_CheckPassword_Character_Under_Limit_Fails()
-        {
-            var infoChecker = new AccountService();
-            Assert.IsFalse(infoChecker.CheckPassword("ss"));
-        }
-
-        [TestMethod]
-        public void Test_CheckPassword_Characters_Not_Permitted()
-        {
-            var infoChecker = new AccountService();
-            Assert.IsFalse(infoChecker.CheckPassword("ÆÆÆ"));
         }
     }
 }

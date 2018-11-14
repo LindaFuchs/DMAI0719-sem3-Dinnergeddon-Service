fa963a3e78c57cd3c5629b35dbe266292fa16d43
Nikola Velichkov <nikola@velichkov-bg.com>
2018-11-14 13:38:57 +0100

    Added DBLayer test project and started testing AccountRepo

---
+++ DBLayer/IAccountRepo.cs
@@ -0,0 +1,18 @@
+﻿using Model;
+using System.Collections.Generic;
+
+namespace DBLayer
+{
+    public interface IAccountRepo
+    {
+        IEnumerable<Account> GetAccounts();
+
+        Account GetAccountByID(int ID);
+        Account GetAccountByUsername(string username);
+        Account GetAccountByEmail(string email);
+
+        int InsertAccount(Account a);
+        int DeleteAccount(Account a);
+        int UpdateAccount(Account oldAccount, Account newAccount);
+    }
+}
---
+++ DBLayer/IDbComponents.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+using System.Data.Common;
+
+namespace DBLayer
+{
+    public interface IDbComponents
+    {
+
+        IDbConnection Connection { get; }
+        DbProviderFactory Factory { get; }
+    }
+}

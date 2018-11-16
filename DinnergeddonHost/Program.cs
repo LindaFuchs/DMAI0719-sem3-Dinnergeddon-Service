3aa528ac043d6eae0e066dc20ef7b91f1d19bc56
Linda <43566477+FoxC1337@users.noreply.github.com>
2018-11-16 10:05:47 +0100

    Renamed Dinnergeddon to DinnergeddonHost and added references

---
+++ DinnergeddonHost/Program.cs
@@ -0,0 +1,38 @@
+﻿using DinnergeddonService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinnergeddonHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8733/DinnergeddonService/");
+
+            ServiceHost host = new ServiceHost(typeof(IAccountService), baseAddress);
+
+            try
+            {
+                host.AddServiceEndpoint(typeof(IAccountService), new BasicHttpBinding(), "AccountService");
+
+                host.Open();
+
+                Console.WriteLine("Service has been started");
+                Console.WriteLine("Press enter to terminate service...");
+                Console.ReadLine();
+
+                host.Close();
+            }
+            catch (CommunicationException e)
+            {
+                Console.WriteLine(e.Message);
+                host.Abort();
+            }
+        }
+    }
+}

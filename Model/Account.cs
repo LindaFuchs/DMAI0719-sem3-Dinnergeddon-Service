db47eb4d67943cd780ce224a9f1db60b3e1a4eb2
Alexander Ignácz <1067661@ucn.dk>
2018-11-16 09:58:02 +0100

    Account Id name changed

--- Model/Account.cs
+++ Model/Account.cs
@@ -3,7 +3,7 @@
 namespace Model {
     public class Account{
 
-        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }

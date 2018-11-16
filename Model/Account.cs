470b28b54a80c7de3784bfdf75c89c6ad77c44cb
Nikola Anastasov Velichkov <nikola@velichkov-bg.com>
2018-11-16 10:02:59 +0100

    Changed bracket to match C# convention

--- Model/Account.cs
+++ Model/Account.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 
-namespace Model {
-    public class Account{
-
+namespace Model
+{
+    public class Account
+    {
         public Guid Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
         public string SecurityStamp { get; set; }
-        
     }
 }

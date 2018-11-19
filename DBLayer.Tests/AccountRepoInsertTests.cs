98961b915154e72c9ce449931a52f82166c41e12
TheRealAnoobis <an00bis@abv.bg>
2018-11-19 11:16:02 +0100

    Unfinished tests

--- DBLayer.Tests/AccountRepoInsertTests.cs
+++ DBLayer.Tests/AccountRepoInsertTests.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Model;
 using System;
-using System.Data;
-using System.Data.Common;
+using static DBLayer.Tests.AccountRepoMocks;
 
 namespace DBLayer.Tests
 {
@@ -78,116 +77,6 @@ namespace DBLayer.Tests
         public void InsertAccountReturnsCorrectNumberOfRowsAffected()
         {
             throw new NotImplementedException();
-        }
-    }
-
-    class DbComponentsMock : IDbComponents
-    {
-        public DbComponentsMock(IDbConnection connection, DbProviderFactory factory)
-        {
-            Connection = connection;
-            Factory = factory;
-        }
-
-        public IDbConnection Connection { get; }
-        public DbProviderFactory Factory { get; }
-    }
-
-    class DbConnectionMock : IDbConnection
-    {
-        public bool connectionOpened;
-        public bool connectionClosed;
-
-        public DbTransactionMock transaction;
-
-        public bool transactionUsed;
-
-        public DbConnectionMock()
-        {
-            connectionOpened = false;
-            connectionClosed = false;
-            transactionUsed = false;
-        }
-
-        public string ConnectionString { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public int ConnectionTimeout => throw new NotImplementedException();
-
-        public string Database => throw new NotImplementedException();
-
-        public ConnectionState State => throw new NotImplementedException();
-
-        public IDbTransaction BeginTransaction()
-        {
-            transaction = new DbTransactionMock();
-            transactionUsed = true;
-            return transaction;
-        }
-
-        public IDbTransaction BeginTransaction(IsolationLevel il)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ChangeDatabase(string databaseName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Close()
-        {
-            connectionClosed = true;
-        }
-
-        public IDbCommand CreateCommand()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Open()
-        {
-            connectionOpened = true;
-        }
-    }
-
-    class DbProviderFactoryMock : DbProviderFactory
-    {
-
-    }
-
-    class DbTransactionMock : IDbTransaction
-    {
-        public bool commited;
-        public bool rollbacked;
-
-        public DbTransactionMock()
-        {
-            commited = false;
-            rollbacked = false;
-        }
-
-        public IDbConnection Connection => throw new NotImplementedException();
-
-        public IsolationLevel IsolationLevel => throw new NotImplementedException();
-
-        public void Commit()
-        {
-            commited = true;
-        }
-
-        public void Dispose()
-        {
-            return;
-        }
-
-        public void Rollback()
-        {
-            rollbacked = true;
         }
     }
 }

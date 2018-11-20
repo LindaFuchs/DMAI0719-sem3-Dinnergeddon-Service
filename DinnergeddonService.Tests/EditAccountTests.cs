3ad91242f3abcd1b026f6c99e2a513e68e3ccdc7
TheRealAnoobis <an00bis@abv.bg>
2018-11-20 10:47:08 +0100

    Actually added mocks, good job me

--- DinnergeddonService.Tests/EditAccountTests.cs
+++ DinnergeddonService.Tests/EditAccountTests.cs
@@ -5,6 +5,7 @@ using DBLayer;
 using System.Data;
 using System.Data.Common;
 using Model;
+using System.Collections.Generic;
 
 namespace DinnergeddonService.Tests
 {
@@ -19,7 +20,7 @@ namespace DinnergeddonService.Tests
         [TestMethod]
         public void Test_EditAccount_All_Correct_Info_Succeeds()
         {
-            var infoChecker = new AccountServiceMock();
+            var infoChecker = new AccountService(new AccountRepoMock());
             Assert.IsTrue(infoChecker.EditAccount(username, email, password));
         }
 
@@ -27,7 +28,7 @@ namespace DinnergeddonService.Tests
         [ExpectedException(typeof(ArgumentException))]
         public void Test_EditAccount_Blank_Username_Throws_Exception()
         {
-            var infoChecker = new AccountServiceMock();
+            var infoChecker = new AccountService(new AccountRepoMock());
             infoChecker.EditAccount("", email, password);
         }
 
@@ -35,7 +36,7 @@ namespace DinnergeddonService.Tests
         [ExpectedException(typeof(ArgumentException))]
         public void Test_EditAccount_Blank_Email_Throws_Exception()
         {
-            var infoChecker = new AccountServiceMock();
+            var infoChecker = new AccountService(new AccountRepoMock());
             infoChecker.EditAccount(username, "", password);
         }
 
@@ -43,35 +44,35 @@ namespace DinnergeddonService.Tests
         [ExpectedException(typeof(ArgumentException))]
         public void Test_EditAccount_Blank_Password_Throws_Exception()
         {
-            var infoChecker = new AccountServiceMock();
+            var infoChecker = new AccountService(new AccountRepoMock());
             infoChecker.EditAccount(username, email, "");
         }
 
         [TestMethod]
         public void Test_CheckEmail_Correct_Info_Passes()
         {
-            var infoChecker = new AccountServiceMock();
+            var infoChecker = new AccountService(new AccountRepoMock());
             Assert.IsTrue(infoChecker.CheckEmail(email));
         }
 
         [TestMethod]
         public void Test_CheckEmail_Invalid_Format_Fails()
         {
-            var infoChecker = new AccountServiceMock();
+            var infoChecker = new AccountService(new AccountRepoMock());
             Assert.IsTrue(infoChecker.CheckEmail("john@mail"));
         }
 
         [TestMethod]
         public void Test_CheckEmail_Blank_Fails()
         {
-            var infoChecker = new AccountServiceMock();
+            var infoChecker = new AccountService(new AccountRepoMock());
             Assert.IsFalse(infoChecker.CheckEmail("  "));
         }
 
         [TestMethod]
         public void Test_CheckUsername_Correct_Info_Passes()
         {
-            var infoChecker = new AccountServiceMock();
+            var infoChecker = new AccountService(new AccountRepoMock());
             Assert.IsTrue(infoChecker.CheckUsername(username));
         }
 
@@ -79,108 +80,108 @@ namespace DinnergeddonService.Tests
         public void Test_CheckUsername_Characters_Over_Limit_Fails()
         {
             //32 char limit
-            var infoChecker = new AccountServiceMock();
+            var infoChecker = new AccountService(new AccountRepoMock());
             Assert.IsFalse(infoChecker.CheckUsername("123456789012345678901234567890123"));
         }
 
         [TestMethod]
         public void Test_CheckUsername_Characters_Under_Limit_Fails()
         {
-            var infoChecker = new AccountServiceMock();
+            var infoChecker = new AccountService(new AccountRepoMock());
             Assert.IsFalse(infoChecker.CheckUsername("us"));
         }
 
         [TestMethod]
         public void Test_CheckUsername_Characters_Not_Permitted_Fails()
         {
-            var infoChecker = new AccountServiceMock();
+            var infoChecker = new AccountService(new AccountRepoMock());
             Assert.IsFalse(infoChecker.CheckUsername("ÆÆ"));
         }
 
         [TestMethod]
         public void Test_CheckUsername_Blank_Fails()
         {
-            var infoChecker = new AccountServiceMock();
+            var infoChecker = new AccountService(new AccountRepoMock());
             Assert.IsFalse(infoChecker.CheckUsername("  "));
         }
 
         [TestMethod]
         public void Test_CheckPassword_CorrectInfo_Passes()
         {
-            var infoChecker = new AccountServiceMock();
+            var infoChecker = new AccountService(new AccountRepoMock());
             Assert.IsTrue(infoChecker.CheckPassword(password));
         }
 
         [TestMethod]
         public void Test_CheckPassword_Character_Over_Limit_Fails()
         {
-            var infoChecker = new AccountServiceMock();
+            var infoChecker = new AccountService(new AccountRepoMock());
             Assert.IsFalse(infoChecker.CheckPassword("1234567890123456789012345678901"));
         }
 
         [TestMethod]
         public void Test_CheckPassword_Character_Under_Limit_Fails()
         {
-            var infoChecker = new AccountServiceMock();
+            var infoChecker = new AccountService(new AccountRepoMock());
             Assert.IsFalse(infoChecker.CheckPassword("ss"));
         }
 
         [TestMethod]
         public void Test_CheckPassword_Characters_Not_Permitted_Fails()
         {
-            var infoChecker = new AccountServiceMock();
+            var infoChecker = new AccountService(new AccountRepoMock());
             Assert.IsFalse(infoChecker.CheckPassword("ÆÆÆ"));
         }
     }
 
-    public class AccountServiceMock : IAccountService
+    public class AccountRepoMock : IAccountRepo
     {
-        public bool CheckEmail(string email)
+        public int AddToRole(Guid accountID, string roleName)
         {
             throw new NotImplementedException();
         }
 
-        public bool CheckPassword(string password)
+        public int DeleteAccount(Account a)
         {
             throw new NotImplementedException();
         }
 
-        public bool CheckUsername(string username)
+        public Account GetAccountByEmail(string email)
         {
             throw new NotImplementedException();
         }
 
-        public bool EditAccount(string username, string email, string password)
+        public Account GetAccountByID(Guid accountID)
         {
             throw new NotImplementedException();
         }
 
-        public Account FindByEmail(string email)
+        public Account GetAccountByUsername(string username)
         {
             throw new NotImplementedException();
         }
 
-        public Account FindById(Guid id)
+        public IEnumerable<Account> GetAccounts()
         {
             throw new NotImplementedException();
         }
 
-        public Account GetInfo()
+        public IEnumerable<string> GetRoles(Guid accountID)
         {
             throw new NotImplementedException();
         }
 
-        public bool InsertAccount(Account account)
+        public int InsertAccount(Account a)
         {
             throw new NotImplementedException();
         }
 
-        public bool Login(string username, string password)
+        public bool IsInsideRole(Guid accountID, string roleName)
         {
             throw new NotImplementedException();
         }
 
-        public bool RegisterAccount(string username, string email, string password)
+        public int UpdateAccount(Account oldAccount, Account newAccount)
         {
             throw new NotImplementedException();
         }

ef3e194f4d0ec1b13e1120e3b34ffdbca347a18b
Nikola Anastasov Velichkov <nikola@velichkov-bg.com>
2018-11-20 10:44:57 +0100

    Added correct reference and exposing Model class library

--- Model/Account.cs
+++ Model/Account.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.ServiceModel;
+using System.Runtime.Serialization;
 
 namespace Model
 {
+    [DataContract]
     public class Account
     {
+        [DataMember]
         public Guid Id { get; set; }
+        [DataMember]
         public string Username { get; set; }
+        [DataMember]
         public string Email { get; set; }
+        [DataMember]
         public string PasswordHash { get; set; }
+        [DataMember]
         public string SecurityStamp { get; set; }
     }
 }

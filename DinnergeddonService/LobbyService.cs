96957d1fa16e55af29add4d2bd1bf4e115014699
Nikola Anastasov Velichkov <nikola@velichkov-bg.com>
2018-11-22 13:10:52 +0100

    Added lobby interfaces and controllers

---
+++ DinnergeddonService/LobbyService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Model;
+
+namespace DinnergeddonService
+{
+    public class LobbyService : ILobbyService
+    {
+        public Lobby CreateLobby(string name, int playerLimit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Lobby CreateLobby(string name, int playerLimit, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

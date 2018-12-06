4f4c67cdbeea1fd3fe585d96ca5cdb7fe302c40c
Nikola Anastasov Velichkov <nikola@velichkov-bg.com>
2018-12-06 14:00:01 +0100

    Updated visibility of new classes

---
+++ Controller/IHighscoreController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    public interface IHighscoreController
+    {
+        /// <summary>
+        /// Gets the highscore of a single account given it's ID
+        /// </summary>
+        /// <param name="accountId">The ID of the account</param>
+        /// <returns>The highscore of that account</returns>
+        int GetHighscore(Guid accountId);
+
+        /// <summary>
+        /// Gets the top N highscores
+        /// </summary>
+        /// <param name="n">The number of highscores to be returned</param>
+        /// <returns>A list of highscores and their respective account IDs</returns>
+        IDictionary<Guid, int> GetHighscores(int n);
+
+        /// <summary>
+        /// Gets all the highscore values
+        /// </summary>
+        /// <returns>A list of highscores and their respective account IDs</returns>
+        IDictionary<Guid, int> GetHighscores();
+    }
+}

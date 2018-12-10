009936ff1626c28060f98d98a337ac0871cba282
TheRealAnoobis <an00bis@abv.bg>
2018-12-10 13:30:37 +0100

    Added (slightly unfinished) file download

--- DinnergeddonWeb/Controllers/HomeController.cs
+++ DinnergeddonWeb/Controllers/HomeController.cs
@@ -61,10 +61,16 @@ namespace DinnergeddonWeb.Controllers
             return View(highScoreModels);
         }
 
-        public ActionResult Download()
+        public FileResult Download()
         {
-
-            return View();
+            //The path to the file to download.
+            //TODO: Change this so it isn't a hard path and to the .exe when it's made.
+            String filePath = @"D:\anoobis\CODE N SHIT\3rd Semester Project\Dinnergeddon\DinnergeddonWeb\images\logo.png";
+            //Converts the file content into an array of bytes.
+            byte[] fileContent = System.IO.File.ReadAllBytes(filePath);
+            
+            //Returns the file content along with the type for the download.
+            return File(fileContent, "application/png");
         }
 
         public ActionResult Contact()

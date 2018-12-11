b1d24f699cb80681259478b4816064e1c81e8189
Nikola Anastasov Velichkov <nikola@velichkov-bg.com>
2018-12-11 13:27:27 +0100

    Removed ovsolete WCF methods

--- DinnergeddonService/Service.svc.cs
+++ DinnergeddonService/Service.svc.cs
@@ -7,7 +7,7 @@ using System.ServiceModel;
 namespace DinnergeddonService
 {
     [ServiceBehavior]
-    public class DinnergeddonService : IAccountService, ILobbyService
+    public class DinnergeddonService : IAccountService
     {
         private readonly IAccountController accountController;
         private readonly ILobbyController lobbyController;
@@ -119,81 +119,6 @@ namespace DinnergeddonService
         public bool DeleteAccount(Account account)
         {
             return accountController.DeleteAccount(account);
-        }
-
-        /// <summary>
-        /// Creates a lobby that is accessible for all players that are online
-        /// </summary>
-        /// <param name="name">The name of the lobby</param>
-        /// <param name="playerLimit">The number of players that can join</param>
-        /// <returns>The new lobby that was created</returns>
-        public Lobby CreateLobby(string name, int playerLimit)
-        {
-            return lobbyController.CreateLobby(name, playerLimit);
-        }
-
-        /// <summary>
-        /// Creates a private lobby that is accessible only for the players that have the password
-        /// </summary>
-        /// <param name="name">The name of the lobby</param>
-        /// <param name="playerLimit">The number of players that can join</param>
-        /// <param name="password">The password of the lobby</param>
-        /// <returns>The new lobby that was created</returns>
-        public Lobby CreatePrivateLobby(string name, int playerLimit, string password)
-        {
-            return lobbyController.CreateLobby(name, playerLimit, password);
-        }
-
-        /// <summary>
-        /// Returns all lobbies that are currently active
-        /// </summary>
-        /// <returns>A list of all the active lobbies</returns>
-        public IEnumerable<Lobby> GetLobbies()
-        {
-            return lobbyController.GetLobbies();
-        }
-
-        /// <summary>
-        /// Adds an account to a lobby
-        /// </summary>
-        /// <param name="accountId">The ID of the account</param>
-        /// <param name="lobbyId">The ID of the lobby</param>
-        /// <returns>If the operation was succesful</returns>
-        public bool JoinLobby(Guid accountId, Guid lobbyId)
-        {
-            return lobbyController.JoinLobby(accountId, lobbyId);
-        }
-
-        /// <summary>
-        /// Removes an account from a lobby
-        /// </summary>
-        /// <param name="accountId">The ID of the account</param>
-        /// <param name="lobbyId">The ID of the lobby</param>
-        public void LeaveLobby(Guid accountId, Guid lobbyId)
-        {
-            lobbyController.LeaveLobby(accountId, lobbyId);
-        }
-
-        /// <summary>
-        /// Adds an account to a private lobby
-        /// </summary>
-        /// <param name="accountId">The account ID</param>
-        /// <param name="lobbyId">The lobby ID</param>
-        /// <param name="password">The password of the lobby</param>
-        /// <returns>If the operation was successful</returns>
-        public bool JoinPrivateLobby(Guid accountId, Guid lobbyId, string password)
-        {
-            return lobbyController.JoinLobby(accountId, lobbyId, password);
-        }
-
-        /// <summary>
-        /// Gets a lobby given an ID
-        /// </summary>
-        /// <param name="lobbyId">The ID of the lobby</param>
-        /// <returns>A lobby object with ID or null</returns>
-        public Lobby GetLobbyById(Guid lobbyId)
-        {
-            return lobbyController.GetLobbyById(lobbyId);
         }
 
         /// <summary>
